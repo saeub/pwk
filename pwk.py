a563d2e81054489b15d8acc3bf6db7111deb083b
Andreas Säuberli <andreas.saeuberli@gmail.com>
2021-12-30 20:59:32 +0100

    Add aggregation mode and header flag

--- pwk.py
+++ pwk.py
@@ -2,29 +2,53 @@
 
 import argparse
 import csv
+import itertools
 import math
+import re
 import sys
 
 
-def process(expr, reader, writer, preprocess, postprocess):
-    for fields in reader:
-        fields = [preprocess(field) for field in fields]
-        globals = {f"_{i}": field for i, field in enumerate([fields] + fields)}
-        globals["_"] = fields
-        globals.update(math.__dict__)
-        # TODO: better way to disallow user stupidity?
-        globals["input"] = None
-        globals["print"] = None
+def evaluate(expr, field_values):
+    globals = {f"_{i}": field for i, field in field_values.items()}
+    globals.update(math.__dict__)
+    # TODO: better way to disallow user stupidity?
+    globals["input"] = None
+    globals["print"] = None
 
-        try:
-            result = eval(expr, globals)
-            if isinstance(result, (tuple, list)):  # TODO: check if iterable
-                result = tuple(postprocess(field) for field in result)
-            else:
-                result = (postprocess(result),)
-        except Exception:
-            result = tuple()
-        writer(result)
+    try:
+        result = eval(expr, globals)
+        if isinstance(result, (tuple, list)):  # TODO: check if iterable
+            result = tuple(field for field in result)
+        else:
+            result = (result,)
+    except Exception:
+        result = tuple()
+    return result
+
+
+def process(expr, rows):
+    for fields in rows:
+        result = evaluate(expr, {i: field for i, field in enumerate([fields] + fields)})
+        yield result
+
+
+def process_aggregate(expr, rows):
+    mentioned_fields = set()
+    for name in expr.co_names:
+        match = re.match(r"^_(\d+)$", name)
+        if match is not None:
+            field_number = int(match.group(1))
+            if field_number > 0:
+                mentioned_fields.add(field_number)
+    field_values = {}
+    row_iterators = itertools.tee(rows, len(mentioned_fields))
+    for field_number, row_iterator in zip(mentioned_fields, row_iterators):
+        field_values[field_number] = [
+            fields[field_number - 1] for fields in row_iterator
+        ]
+
+    result = evaluate(expr, field_values)
+    yield result
 
 
 def preprocess_nothing(field):
@@ -42,7 +66,8 @@ def preprocess_numbers(field):
 
 
 def parse_arguments(cmdargs=None):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--help", action="help")
 
     def compile_expr(expr):
         return compile(expr, "pwk_expr", "eval")
@@ -55,6 +80,7 @@ def parse_arguments(cmdargs=None):
 
     parser.add_argument("-s", dest="string_numbers", action="store_true")
     parser.add_argument("-a", dest="aggregate", action="store_true")
+    parser.add_argument("-h", dest="header", action="store_true")
 
     args = parser.parse_args(cmdargs)
 
@@ -98,7 +124,18 @@ def main(cmdargs, out):
             out.write("\t".join(fields))
             out.write("\n")
 
-    process(args.expr, reader, writer, preprocess, postprocess)
+    if args.header:
+        next(reader)
+
+    rows = ([preprocess(field) for field in fields] for fields in reader)
+
+    if args.aggregate:
+        processor = process_aggregate(args.expr, rows)
+    else:
+        processor = process(args.expr, rows)
+
+    for fields in processor:
+        writer([postprocess(field) for field in fields])
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -5,16 +5,23 @@ import pytest
 import pwk
 
 
-@pytest.mark.parametrize(
-    "filename,expression", [("test", "_1 * 2, _2.upper(), _3 + '?'")]
-)
 @pytest.mark.parametrize("input_format", ["csv", "tsv"])
 @pytest.mark.parametrize("output_format", ["csv", "tsv"])
-def test_files(filename, expression, input_format, output_format):
-    input_filename = f"test_files/{filename}.{input_format}"
-    output_filename = f"test_files/{filename}_output.{output_format}"
+def test_file_formats(input_format, output_format):
+    input_filename = f"test_files/test.{input_format}"
+    output_filename = f"test_files/test_output.{output_format}"
     output = StringIO()
-    pwk.main([expression, input_filename, "-o", output_format], output)
+    pwk.main(
+        ["_1 * 2, _2.upper(), _3 + '?'", input_filename, "-o", output_format], output
+    )
     with open(output_filename, newline="") as output_file:
+        expected_output = output_file.read()
+    assert output.getvalue() == expected_output
+
+
+def test_aggregate():
+    output = StringIO()
+    pwk.main(["-a", "sum(_2)", "test_files/aggregate.csv", "-h", "-o", "csv"], output)
+    with open("test_files/aggregate_output.csv", newline="") as output_file:
         expected_output = output_file.read()
     assert output.getvalue() == expected_output
